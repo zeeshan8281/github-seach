260d153dda8f86311740892a75e094cdce4417b6
Mohammad Zeeshan Jawed <zeeshan8281@gmail.com>
2023-04-10 15:55:55 +0530

    update

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+import Search from './components/Search';
+import './index.css';
+
+
+const App = () => {
+  return (
+    <Container>
+      <Search />
+    </Container>
+  );
+}
+
+export default App;
+
---
+++ src/components/Search.js
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import { Form, Button, ListGroup } from 'react-bootstrap';
+import axios from 'axios';
+import '../index.css'; // import the CSS file
+
+const Search = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [repos, setRepos] = useState([]);
+  const [sortOrder, setSortOrder] = useState('stars');
+
+  const handleSearch = async (e) => {
+    e.preventDefault();
+    const response = await axios.get(`https://api.github.com/users/${searchTerm}/repos?sort=${sortOrder}`);
+    setRepos(response.data);
+  }
+
+  const handleSortOrder = (e) => {
+    setSortOrder(e.target.value);
+  }
+
+  return (
+    <div className="search-container">
+      <Form className="search-form" onSubmit={handleSearch}>
+        <Form.Label>Search for a GitHub user</Form.Label>
+        <Form.Control type="text" placeholder="Enter username" value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} />
+        <Form.Label>Sort by</Form.Label>
+        <Form.Control as="select" value={sortOrder} onChange={handleSortOrder}>
+          <option value="stars">Stars</option>
+          <option value="forks">Forks</option>
+        </Form.Control>
+        <Button type="submit">Search</Button>
+      </Form>
+      <div className="repos-container">
+        {repos.map((repo) => (
+          <ListGroup.Item key={repo.id} className="repo-item">
+            <a href={repo.html_url} target="_blank" rel="noopener noreferrer">{repo.name}</a>
+            <p>{repo.description}</p>
+            <p>Stars: {repo.stargazers_count}</p>
+            <p>Forks: {repo.forks_count}</p>
+          </ListGroup.Item>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Search;
